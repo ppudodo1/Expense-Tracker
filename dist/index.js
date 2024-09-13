3f483b8c0aa2fed068080f1694a12aa476502803
ppudodo <dkatavic03@gmail.com>
2024-09-13 10:54:22 +0200

    Starting to add features

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("Hello world");
+document.addEventListener("DOMContentLoaded", () => {
+    const storedUsername = localStorage.getItem("username");
+    let buttonSubmit = document.querySelector(".user-button");
+    const greetUser = (username) => {
+        alert(`Hello ${username}`);
+    };
+    const saveUsernameToStorage = (username) => {
+        localStorage.setItem("username", username);
+    };
+    if (storedUsername) {
+        let userNameInput = document.querySelector(".username-input");
+        userNameInput === null || userNameInput === void 0 ? void 0 : userNameInput.classList.add("logged-in");
+    }
+    buttonSubmit === null || buttonSubmit === void 0 ? void 0 : buttonSubmit.addEventListener("click", () => {
+        let usernameValue = document.querySelector(".username");
+        greetUser(usernameValue.value);
+        saveUsernameToStorage(usernameValue.value);
+        window.location.reload();
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello world");
+document.addEventListener("DOMContentLoaded", () => {
+  const storedUsername: string | null = localStorage.getItem("username");
+  let buttonSubmit = document.querySelector(".user-button");
+  //console.log("Button", buttonSubmit);
+  const greetUser = (username: string): void => {
+    alert(`Hello ${username}`);
+  };
+  const saveUsernameToStorage = (username: string): void => {
+    localStorage.setItem("username", username);
+  };
+  if (storedUsername) {
+    let userNameInput = document.querySelector(".username-input");
+    userNameInput?.classList.add("logged-in");
+  }
+  buttonSubmit?.addEventListener("click", () => {
+    let usernameValue = document.querySelector(".username") as HTMLInputElement;
+    greetUser(usernameValue.value);
+    saveUsernameToStorage(usernameValue.value);
+    window.location.reload();
+  });
+});
