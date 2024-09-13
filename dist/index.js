085bb41d9e25d05cf297ebf067213134b609aa19
ppudodo <dkatavic03@gmail.com>
2024-09-13 14:04:41 +0200

    Working on new features

--- dist/index.js
+++ dist/index.js
@@ -2,20 +2,46 @@
 document.addEventListener("DOMContentLoaded", () => {
     const storedUsername = localStorage.getItem("username");
     let buttonSubmit = document.querySelector(".user-button");
+    let submitExpenseButton = document.querySelector(".expense-button");
     const greetUser = (username) => {
         alert(`Hello ${username}`);
     };
     const saveUsernameToStorage = (username) => {
         localStorage.setItem("username", username);
     };
-    if (storedUsername) {
-        let userNameInput = document.querySelector(".username-input");
-        userNameInput === null || userNameInput === void 0 ? void 0 : userNameInput.classList.add("logged-in");
-    }
+    const checkForUser = () => {
+        if (storedUsername) {
+            let userNameInput = document.querySelector(".username-input");
+            let greeting = document.querySelector(".show-greeting");
+            let greetingText = document.querySelector(".greeting");
+            if (greetingText) {
+                greetingText.innerHTML = `Hi ${storedUsername}`;
+            }
+            greeting === null || greeting === void 0 ? void 0 : greeting.classList.remove("no-show");
+            userNameInput === null || userNameInput === void 0 ? void 0 : userNameInput.classList.add("logged-in");
+        }
+        else {
+            let expenseForm = document.querySelector(".expense-form");
+            if (expenseForm) {
+                expenseForm.style.display = "none";
+            }
+        }
+    };
+    checkForUser();
+    const createAnExpenseDiv = (expenseData) => {
+    };
     buttonSubmit === null || buttonSubmit === void 0 ? void 0 : buttonSubmit.addEventListener("click", () => {
         let usernameValue = document.querySelector(".username");
         greetUser(usernameValue.value);
         saveUsernameToStorage(usernameValue.value);
         window.location.reload();
+    });
+    submitExpenseButton === null || submitExpenseButton === void 0 ? void 0 : submitExpenseButton.addEventListener("click", () => {
+        let expenseNameValue = document.querySelector(".expense-name");
+        let expenseAmountValue = document.querySelector(".expense-amount");
+        createAnExpenseDiv({
+            expenseName: expenseNameValue.value,
+            expenseAmount: Number(expenseAmountValue.value),
+        });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
+  interface Expense {
+    expenseName: string;
+    expenseAmount: number;
+  }
   const storedUsername: string | null = localStorage.getItem("username");
   let buttonSubmit = document.querySelector(".user-button");
-  //console.log("Button", buttonSubmit);
+  let submitExpenseButton = document.querySelector(".expense-button");
   const greetUser = (username: string): void => {
     alert(`Hello ${username}`);
   };
   const saveUsernameToStorage = (username: string): void => {
     localStorage.setItem("username", username);
   };
-  if (storedUsername) {
-    let userNameInput = document.querySelector(".username-input");
-    userNameInput?.classList.add("logged-in");
-  }
+  const checkForUser = () => {
+    if (storedUsername) {
+      let userNameInput = document.querySelector(".username-input");
+      let greeting = document.querySelector(".show-greeting");
+      let greetingText = document.querySelector(".greeting");
+      if (greetingText) {
+        greetingText.innerHTML = `Hi ${storedUsername}`;
+      }
+      greeting?.classList.remove("no-show");
+      userNameInput?.classList.add("logged-in");
+    } else {
+      let expenseForm = document.querySelector(".expense-form") as HTMLElement;
+      if (expenseForm) {
+        expenseForm.style.display = "none";
+      }
+    }
+  };
+  checkForUser();
+  const createAnExpenseDiv = (expenseData: Expense) => {
+    //console.log(exp)
+  };
   buttonSubmit?.addEventListener("click", () => {
+    // Type assertion to specify more specific type, given that the TypeScript
+    // Initially doesn't know what type of HTML element is being returned here
     let usernameValue = document.querySelector(".username") as HTMLInputElement;
     greetUser(usernameValue.value);
     saveUsernameToStorage(usernameValue.value);
     window.location.reload();
+  });
+  submitExpenseButton?.addEventListener("click", () => {
+    let expenseNameValue = document.querySelector(
+      ".expense-name"
+    ) as HTMLInputElement;
+    let expenseAmountValue = document.querySelector(
+      ".expense-amount"
+    ) as HTMLInputElement;
+    createAnExpenseDiv({
+      expenseName: expenseNameValue.value,
+      expenseAmount: Number(expenseAmountValue.value),
+    });
+    //console.log(expenseNameValue.value);
+    //console.log(expenseAmountValue.value);
   });
 });
